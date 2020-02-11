6b0575bd1217d641db6fd691b2445b10b8e46033
Florian Kempenich <Flori@nKempenich.com>
2020-02-11 17:44:38 +0000

    Add Main to test out Account

--- src/main/java/BankKataJava/Console.java
+++ src/main/java/BankKataJava/Console.java
@@ -2,6 +2,6 @@ package BankKataJava;
 
 public class Console {
     public void printLine(String lineToPrint) {
-        throw new RuntimeException("Not Yet Implemented");
+        System.out.println(lineToPrint);
     }
 }
---
+++ src/main/java/BankKataJava/Main.java
@@ -0,0 +1,22 @@
+package BankKataJava;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Console console = new Console();
+
+        Calendar calendar = new Calendar();
+        TransactionRepo transactionRepo = new TransactionRepo();
+        StatementPrinter statementPrinter = new StatementPrinter(console);
+
+        Account account = new Account(calendar, transactionRepo, statementPrinter);
+
+        account.deposit(190);
+        account.deposit(592);
+        account.withdraw(82);
+        account.deposit(10);
+        account.withdraw(300);
+
+        account.printStatement();
+    }
+}
