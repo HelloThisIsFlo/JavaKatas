c31e66439209c429d9fc070db2bd1eda8bdbbc36
Florian Kempenich <Flori@nKempenich.com>
2020-02-11 23:02:30 +0000

    IntelliJ config update

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,29 +7,21 @@
  */
 
 plugins {
-    // Apply the java plugin to add support for Java
     java
-
-    // Apply the application plugin to add support for building a CLI application.
     application
 }
 
 repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
     jcenter()
 }
 
 dependencies {
-    // This dependency is used by the application.
     implementation("com.google.guava:guava:28.1-jre")
 
     testImplementation("org.mockito:mockito-core:2.+")
-    // Use JUnit test framework
     testImplementation("junit:junit:4.12")
 }
 
 application {
-    // Define the main class for the application.
     mainClassName = "katas.App"
 }
