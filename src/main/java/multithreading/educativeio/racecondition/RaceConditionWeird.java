ca84ea1bd8e678ebe6ba898416ad0a6a353d107f
Florian Kempenich <Flori@nKempenich.com>
2022-12-27 20:25:19 +0000

    Experiment

--- src/main/java/multithreading/educativeio/racecondition/RaceConditionWeird.java
+++ src/main/java/multithreading/educativeio/racecondition/RaceConditionWeird.java
@@ -3,12 +3,13 @@ package multithreading.educativeio.racecondition;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+@SuppressWarnings("ALL")
 class RaceConditionWeird {
 
   //  public static final int MAX = 10_000;
-  public static final int MAX = Integer.MAX_VALUE;
-  public long number = 0;
-  CyclicBarrier barrier = new CyclicBarrier(2);
+//  public static final int MAX = Integer.MAX_VALUE;
+  public long number = Long.MIN_VALUE;
+  CyclicBarrier barrier = new CyclicBarrier(3);
 
   public static void main(String[] args) throws InterruptedException {
     System.out.println("""
@@ -21,55 +22,68 @@ class RaceConditionWeird {
             
         """);
     RaceConditionWeird raceConditionWeird = new RaceConditionWeird();
-    boolean weirdScenarioHasNOTOcurred = true;
-    while (weirdScenarioHasNOTOcurred) {
-      raceConditionWeird.runTest();
-      weirdScenarioHasNOTOcurred = (raceConditionWeird.number == Long.MIN_VALUE
-                                    || raceConditionWeird.number == Long.MAX_VALUE);
-    }
-    System.out.println(raceConditionWeird.number);
+
+    raceConditionWeird.runDemo();
+//    boolean weirdScenarioHasNOTOcurred = true;
+//    while (weirdScenarioHasNOTOcurred) {
+//      raceConditionWeird.runTest();
+//      weirdScenarioHasNOTOcurred = (raceConditionWeird.number == Long.MIN_VALUE
+//                                    || raceConditionWeird.number == Long.MAX_VALUE);
+//    }
+//    System.out.println(raceConditionWeird.number);
   }
 
-  public void runTest() throws InterruptedException {
+  public void runDemo() throws InterruptedException {
     Thread thread1 = new Thread(this::setNumberToMaxValue);
     Thread thread2 = new Thread(this::setNumberToMinValue);
+    Thread thread3 = new Thread(this::keepReadingUntilValueIsntWhatWeExpect);
 
+    thread1.setDaemon(true);
+    thread2.setDaemon(true);
     thread1.start();
     thread2.start();
+    thread3.start();
 
-    thread1.join();
-    thread2.join();
+    thread3.join();
   }
 
   void setNumberToMaxValue() {
-    try {
-      barrier.await();
-    } catch (InterruptedException | BrokenBarrierException e) {
-      throw new RuntimeException(e);
-    }
-    for (int i = 0; i < MAX; i++) {
-//      try {
-//        Thread.sleep(1);
-//      } catch (InterruptedException e) {
-//        throw new RuntimeException(e);
-//      }
+    waitForBarrier();
+    while (true) {
+      Thread.yield();
       number = Long.MAX_VALUE;
     }
   }
 
   void setNumberToMinValue() {
+    waitForBarrier();
+    while (true) {
+      Thread.yield();
+      number = Long.MIN_VALUE;
+    }
+  }
+
+  private void keepReadingUntilValueIsntWhatWeExpect() {
+    waitForBarrier();
+    long yo;
+//    while (yo := number == Long.MIN_VALUE || number == Long.MAX_VALUE) {
+    while ((yo = number) == Long.MIN_VALUE || yo == Long.MAX_VALUE) {
+      //noinspection BusyWait
+      Thread.yield();
+    }
+    System.out.println("WOOOOOOOHOOOOOOOOO We found the BUG !!!");
+    System.out.println("Long.MIN_VALUE = " + Long.MIN_VALUE);
+    System.out.println("Long.MAX_VALUE = " + Long.MAX_VALUE);
+    System.out.println("number = " + yo);
+
+  }
+
+  private void waitForBarrier() {
     try {
       barrier.await();
     } catch (InterruptedException | BrokenBarrierException e) {
-      throw new RuntimeException(e);
+      e.printStackTrace();
     }
-    for (int i = 0; i < MAX; i++) {
-//      try {
-//        Thread.sleep(1);
-//      } catch (InterruptedException e) {
-//        throw new RuntimeException(e);
-//      }
-      number = Long.MIN_VALUE;
-    }
+    System.out.println("Barrier unlocked! Starting now!");
   }
 }
