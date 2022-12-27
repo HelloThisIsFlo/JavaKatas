ddff854ab2805ada6f2cb60b869e0948f775ee35
Florian Kempenich <Flori@nKempenich.com>
2022-12-27 21:24:38 +0000

    Use ExecutorService and Future instead of direct Threads

--- src/main/java/multithreading/educativeio/ThreadSandbox.java
+++ src/main/java/multithreading/educativeio/ThreadSandbox.java
@@ -1,68 +1,67 @@
 package multithreading.educativeio;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ThreadSandbox {
 
 
-    long counter = 0;
+  long counter = 0;
+  ExecutorService executorService;
 
-    public void sum(long start, long end) {
-        for (long i = start; i < end; i++) {
-            counter += i;
-        }
+  public static void main(String[] args) throws Exception {
+    new ThreadSandbox().runSandbox();
+  }
+
+  public void runSandbox() throws Exception {
+    executorService = Executors.newFixedThreadPool(20);
+
+    benchmark("multi", this::multiThreaded);
+    benchmark("multi", this::multiThreaded);
+    benchmark("multi", this::multiThreaded);
+    benchmark("multi", this::multiThreaded);
+    benchmark("single", this::singleThreaded);
+    benchmark("single", this::singleThreaded);
+    benchmark("single", this::singleThreaded);
+
+
+  }
+
+  private void benchmark(String tag, RunnableWithException runnable) throws Exception {
+    long start = System.nanoTime() / 1_000_000;
+    runnable.run();
+    long end = System.nanoTime() / 1_000_000;
+    System.out.println(tag);
+    System.out.println("Time taken: " + (end - start));
+  }
+
+  private void multiThreaded() throws Exception {
+    ThreadSandbox sum1stHalf = new ThreadSandbox();
+    ThreadSandbox sum2ndHalf = new ThreadSandbox();
+
+    Future<String> firstHalfDone =
+        executorService.submit(() -> sum1stHalf.sum(0, Integer.MAX_VALUE / 2), "Done");
+    Future<String> secondHalfDone = executorService.submit(
+        () -> sum2ndHalf.sum(Integer.MAX_VALUE / 2, Integer.MAX_VALUE),
+        "Done"
+    );
+    firstHalfDone.get();
+    secondHalfDone.get();
+  }
+
+  private void singleThreaded() throws Exception {
+    ThreadSandbox sumComplete = new ThreadSandbox();
+    Future<String> done =
+        executorService.submit(() -> sumComplete.sum(0, Integer.MAX_VALUE), "done");
+    done.get();
+  }
+
+  public void sum(long start, long end) {
+    for (long i = start; i < end; i++) {
+      counter += i;
     }
-
-    public static void main(String[] args) throws Exception {
-        new ThreadSandbox().runSandbox();
-    }
-
-
-    public void runSandbox() throws Exception {
-        benchmark("multi", ThreadSandbox::multiThreaded);
-        benchmark("multi", ThreadSandbox::multiThreaded);
-        benchmark("multi", ThreadSandbox::multiThreaded);
-        benchmark("multi", ThreadSandbox::multiThreaded);
-        benchmark("single", ThreadSandbox::singleThreaded);
-        benchmark("single", ThreadSandbox::singleThreaded);
-        benchmark("single", ThreadSandbox::singleThreaded);
-
-
-    }
-
-    private static void multiThreaded() throws Exception{
-        ThreadSandbox sum1stHalf = new ThreadSandbox();
-        ThreadSandbox sum2ndHalf = new ThreadSandbox();
-
-        Thread thread1 = new Thread(() -> {
-            sum1stHalf.sum(0, Integer.MAX_VALUE / 2);
-        });
-        Thread thread2 = new Thread(() -> {
-            sum2ndHalf.sum(Integer.MAX_VALUE / 2, Integer.MAX_VALUE);
-        });
-
-        thread1.start();
-        thread2.start();
-        thread1.join();
-        thread2.join();
-
-        double result = sum1stHalf.counter + sum2ndHalf.counter;
-    }
-
-    private static void singleThreaded() throws Exception {
-        ThreadSandbox sumComplete = new ThreadSandbox();
-        Thread thread = new Thread(() -> {
-
-            sumComplete.sum(0, Integer.MAX_VALUE);
-        });
-        thread.start();
-        thread.join();
-        double result = sumComplete.counter;
-    }
-
-    private void benchmark(String tag, RunnableWithException runnable) throws Exception {
-        long start = System.nanoTime() / 1_000_000;
-        runnable.run();
-        long end = System.nanoTime() / 1_000_000;
-        System.out.println(tag);
-        System.out.println("Time taken: " + (end - start));
-    }
+  }
 }
